20ad1753bffc8fc0cff44c453bee4df049aeabc0
BayezidMostafa <hiperx1@gmail.com>
2022-10-26 20:59:24 +0600

    Implemented Details Info About Every Course

--- src/components/CourseDetails/CourseDetails.js
+++ src/components/CourseDetails/CourseDetails.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import { Link, useLoaderData } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
+import { BiDownload } from "react-icons/bi";
 
 const CourseDetails = () => {
     const data = useLoaderData()
     const { about, course_name, mentor, cover_thumb, duration, price } = data;
     console.log(data);
+
+    const downloadHandler = () => {
+        console.log('clicked');
+    }
+
     return (
-        <div className="p-4 shadow-md dark:bg-gray-900 dark:text-gray-100 md:container mx-auto">
-            <p className='text-4xl'>{course_name}</p>
+        <div className="p-6 shadow-md dark:bg-gray-900 dark:text-gray-100 md:container mx-auto my-5 rounded-md">
+            <div className='flex justify-between mb-5 items-center'>
+                <p className='font-bold text-xl md:text-4xl'>{course_name}</p>
+                <BiDownload onClick={downloadHandler} className='text-3xl cursor-pointer' />
+            </div>
             <div className="space-y-4">
                 <div className="space-y-2">
-                    <img src={cover_thumb} alt="" className="block object-cover object-center w-3/4 mx-auto rounded-md dark:bg-gray-500" />
-                    <div className="flex items-center text-xs">
-                        <span>6 min ago</span>
-                    </div>
+                    <img src={cover_thumb} alt="" className="block object-cover object-center w-4/4 mx-auto rounded-md dark:bg-gray-500" />
+                </div>
+                <div>
+                    <p>Duration: {duration} Minute</p>
+                    <p>Fee: ${price}</p>
                 </div>
                 <div className="space-y-2">
-                    <Link rel="noopener noreferrer" href="#" className="block">
-                        <h3 className="text-xl font-semibold dark:text-violet-400">Facere ipsa nulla corrupti praesentium pariatur architecto</h3>
-                    </Link>
-                    <p className="leading-snug dark:text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, excepturi. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, excepturi.</p>
+                    <p>{about}</p>
+                    <p></p>
                 </div>
             </div>
         </div>
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SideBar = ({ singleCourseData }) => {
+    const {course_name, id} = singleCourseData
     return (
         <div className="flex flex-col h-full p-5 w-56 dark:bg-gray-900 dark:text-gray-100">
-            <Link className='bg-gray-800 py-2 hover:bg-gray-700 text-center rounded'>{singleCourseData.course_name}</Link>
+            <Link to={`/courses/${id}`} className='bg-gray-800 py-2 hover:bg-gray-700 text-center rounded'>{course_name}</Link>
         </div>
     );
 };
